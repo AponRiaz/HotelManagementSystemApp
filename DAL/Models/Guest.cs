680fdb6a1b1d80bc2674db95511e096cd4bad6cc
20-42905-1 <aponriaze12022000@gmail.com>
2023-12-24 16:18:02 +0600

    Done DAL Model  Class

--- DAL/Models/Guest.cs
+++ DAL/Models/Guest.cs
@@ -1,21 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL.Models
 {
-    public class Staff
+    public class Guest
     {
         [Key]
-        public int StaffID { get; set; }
-
-
-        [ForeignKey("Hotel")]
-        public int HotelID { get; set; }
+        public int GuestID { get; set; }
 
         [Required]
         public string FirstName { get; set; }
@@ -24,24 +19,15 @@ namespace DAL.Models
         public string LastName { get; set; }
 
         [Required]
-        public string Position { get; set; }
-
-        [Required]
-        public int Salary { get; set; }
-
-        [Required]
         public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public string Address { get; set; }
 
         [Required]
         public string Phone { get; set; }
 
         [Required]
         public string Email { get; set; }
-
-        [Required]
-        public DateTime HireDate { get; set; }
-
-        public virtual Hotel Hotel { get; set; }
     }
-
 }
